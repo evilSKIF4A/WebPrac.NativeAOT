1bccb433f376a3e4803212cdd878eb16a7eb4cdd
evilSKIF4A <skifcha200211@mail.ru>
2026-03-01 15:22:55 +0500

    custom log formatter

--- WebPrac.NativeAOT.Api/Program.cs
+++ WebPrac.NativeAOT.Api/Program.cs
@@ -1,11 +1,26 @@
 using System.Diagnostics;
 using WebPrac.NativeAOT.Api.Endpoints;
+using WebPrac.NativeAOT.Application;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerTemperaturesContext.Default);
+});
+
+builder.Services.AddLogging(config =>
+{
+    config.ClearProviders();
+
+    config.AddConsoleFormatter<LogFormatter, LogFormatterOptions>();
+    
+    config.Services.Configure<LogFormatterOptions>(options =>
+    {
+        options.Template = "{Timestamp} [{Level}] {Category}: {Message}";
+    });
+
+    config.SetMinimumLevel(LogLevel.Debug);
 });
 
 builder.Services.AddOpenApi();
@@ -20,8 +35,12 @@ if (app.Environment.IsDevelopment())
 // Minimal APIs
 app.AddTemperatureGroup();
 
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+
 app.Use(async (_, next) =>
 {
+    logger.LogDebug("Starting web host");
     var time = Stopwatch.StartNew();
     await next();
     time.Stop();
---
+++ WebPrac.NativeAOT.Application/LogFormatter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
+
+namespace WebPrac.NativeAOT.Application;
+
+public sealed class LogFormatter : ConsoleFormatter
+{
+    private readonly LogFormatterOptions _options;
+
+    public LogFormatter(IOptions<LogFormatterOptions> options) : base(nameof(LogFormatter))
+    {
+        _options = options?.Value ?? new LogFormatterOptions();
+    }
+
+    public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider? scopeProvider, TextWriter textWriter)
+    {
+        if (textWriter == null) return;
+
+        var timestamp = DateTime.UtcNow.ToString("o");
+        var level = logEntry.LogLevel.ToString().ToUpperInvariant();
+        var category = logEntry.Category ?? "";
+        var message = logEntry.Formatter?.Invoke(logEntry.State, logEntry.Exception) ?? "";
+
+        var sb = new StringBuilder();
+        sb.Append(_options.Template
+            .Replace("{Timestamp}", timestamp)
+            .Replace("{Level}", level)
+            .Replace("{Category}", category)
+            .Replace("{Message}", message));
+
+        if (logEntry.Exception != null)
+        {
+            sb.Append(" | EX: ").Append(logEntry.Exception);
+        }
+
+        textWriter.WriteLine(sb.ToString());
+    }
+}
+
+public sealed class LogFormatterOptions : ConsoleFormatterOptions
+{
+    public string Template { get; set; } = "{Timestamp} [{Level}] {Category}: {Message}";
+}
